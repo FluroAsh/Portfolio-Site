27697294d0d50a9d3a50e9b4d2aae42c9aa67135
FluroAsh <workashleythompson@gmail.com>
2021-11-04 18:25:28 +1100

    1. Fixed scroll behaviour 2. Added functional footer with 'to-top' button 3. Added border to footer for visual seperation between main body 4. Adjusted some margins/padding

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,18 @@ menuBtn.addEventListener('click', function () {
     menuBtn.classList.toggle('open');
     mobile_menu.classList.toggle('open');
 })
+
+var btn = $('#button');
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+    } else {
+        btn.removeClass('show');
+    }
+});
+
+btn.on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, '300');
+});
